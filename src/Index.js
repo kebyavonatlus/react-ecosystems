ac5a33086f016f2968b847adfd5e22daad0e04ee
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-01-23 14:49:42 +0600

    add react-redux config

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './store';
 import App from './App';
 
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+    <Provider store={ConfigureStore()} >
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
